fe93a4e7361bca2a01f461e27a40dc994afc78c4
Renuka <renuanba@outlook.com>
2026-07-24 17:10:39 +0530

    day 2 completed

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,34 @@
 bmi = 84 / 1.65 ** 2
+
+# float
+print(bmi)
+
+# int
+print(int(bmi))
+
+# round
+print(round(bmi))
+print(round(bmi, 2))
+
+# assignment operators
+score = 132
+print(score)
+score += 35
+print(score)
+score -= 57
+print(score)
+score *= 2
+print(score)
+score //= 15
+print(score)
+score %= 3
+print(score)
+score **= 4
+print(score)
+score /= 5
+print(score)
+
+# without f strings
+print("Score:" + str(score))
+# f strings
+print(f"Score:{score}")
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,4 +3,6 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+bill_per_person = (bill / people) * (1 + tip / 100)
+print(f"Each person should pay: ${round(bill_per_person, 2)}")
 
