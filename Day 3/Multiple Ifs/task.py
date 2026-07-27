001901100b35c460944e037cf76894a43285391d
Renuka <renuanba@outlook.com>
2026-07-27 20:45:03 +0530

    day 3 partial

--- Day 3/Multiple Ifs/task.py
+++ Day 3/Multiple Ifs/task.py
@@ -1,14 +1,25 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age <= 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Children ticket is $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth ticket is $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult ticket is $12.")
+
+    want_photos = input("Do you want photos? (y/n) ")
+    if want_photos == "y":
+        bill += 3
+
+    print(f"Total bill is ${bill}.")
+
 else:
     print("Sorry you have to grow taller before you can ride.")
