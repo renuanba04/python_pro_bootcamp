a330fa6d0b68d20577f5e50bb2fcbdc38015f4ce
Renuka <renuanba@outlook.com>
2026-08-24 07:35:53 -0400

    day 5 completed

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,6 @@
 fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+    print(fruits) # runs this code inside the loop
+print(fruits) # runs after the loop
--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,22 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 print(range(1, 10))
+
+total_score = sum(student_scores)
+print(f"Total Score using sum(): {total_score}")
+
+# using for loops
+total = 0
+for score in student_scores:
+    total += score
+
+print(f"Total score using for loop: {total}")
+
+max_score = max(student_scores)
+print(f"Maximum score using max(): {max_score}")
+
+max_score = 0
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+
+print(f"Maximum score using for loops: {max_score}")
